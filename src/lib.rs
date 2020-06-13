d029a9f01cf273dd50cf45fc78f3603c1e8883ef
Brendan Molloy <brendan@bbqsrc.net>
2020-06-13 15:08:12 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,27 @@
 
 use std::sync::{
     atomic::{AtomicU128, Ordering},
-    Arc,
 };
-use std::{marker::PhantomData, ptr::NonNull};
+use std::{marker::PhantomData, ptr::NonNull, fmt::{Debug, Display}, ops::Deref};
+
+
+#[repr(transparent)]
+pub struct ArcRef<T> {
+    ptr: NonNull<ArcCellInner<T>>,
+    phantom: PhantomData<ArcCellInner<T>>,
+}
 
 pub struct ArcCell<T> {
     ptr: NonNull<ArcCellInner<T>>,
     phantom: PhantomData<ArcCellInner<T>>,
 }
 
-unsafe impl<T: Sync + Send> Send for ArcCell<T> {}
-unsafe impl<T: Sync + Send> Sync for ArcCell<T> {}
-
 #[repr(transparent)]
 /// (strong: u64, ptr: u64)
 struct ArcCellInner<T: ?Sized>(AtomicU128, PhantomData<T>);
+
+unsafe impl<T: Sync + Send> Send for ArcCell<T> {}
+unsafe impl<T: Sync + Send> Sync for ArcCell<T> {}
 
 impl<T> ArcCellInner<T> {
     const MASK_STRONG: u128 = 0xFFFF_FFFF_FFFF_FFFF_0000_0000_0000_0000;
@@ -35,7 +41,7 @@ impl<T> ArcCellInner<T> {
     }
 
     #[inline(always)]
-    fn set_ptr_null(&self) -> (u64, *const T) {
+    fn set_ptr_null(&self) -> (u64, *mut T) {
         loop {
             let current = self.0.load(Ordering::Relaxed);
             let new = current & !Self::MASK_PTR;
@@ -45,7 +51,7 @@ impl<T> ArcCellInner<T> {
                     .compare_exchange(current, new, Ordering::Release, Ordering::Relaxed)
             {
                 let strong = (value & Self::MASK_STRONG >> 64) as u64;
-                let ptr = (value & Self::MASK_PTR) as *const T;
+                let ptr = (value & Self::MASK_PTR) as *mut T;
 
                 return (strong, ptr);
             }
@@ -85,7 +91,7 @@ impl<T> ArcCellInner<T> {
     }
 
     #[inline(always)]
-    fn set_ptr(&self, ptr: *const T) -> *const T {
+    fn set_ptr(&self, ptr: *mut T) -> *mut T {
         loop {
             let current = self.0.load(Ordering::Relaxed);
             let new = (current & !Self::MASK_PTR) | ptr as u128;
@@ -95,7 +101,7 @@ impl<T> ArcCellInner<T> {
                     .compare_exchange(current, new, Ordering::Release, Ordering::Relaxed)
             {
                 // println!("-- Set ptr");
-                return (value & Self::MASK_PTR) as usize as *const T;
+                return (value & Self::MASK_PTR) as usize as *mut T;
             }
         }
     }
@@ -120,7 +126,7 @@ impl<T> Drop for ArcCell<T> {
         let (_, ptr) = self.inner().set_ptr_null();
         // println!("-- Dropping {:x}", ptr as usize);
 
-        drop(unsafe { Arc::from_raw(ptr) });
+        drop(unsafe { Box::from_raw(ptr) });
 
         // We can deallocate the inner pointer now
         // println!("-- Dropping inner");
@@ -132,25 +138,23 @@ impl<T> Drop for ArcCell<T> {
 
 impl<T> ArcCell<T> {
     #[inline]
-    pub fn new(data: Arc<T>) -> ArcCell<T> {
-        let x = Box::new(ArcCellInner::new(Arc::into_raw(data)));
+    pub fn new(data: Box<T>) -> ArcCell<T> {
+        let x = Box::new(ArcCellInner::new(Box::into_raw(data)));
 
         Self::from_inner(unsafe { NonNull::new_unchecked(Box::into_raw(x) as *mut _) })
     }
 
     /// Returns old data
     #[inline]
-    pub fn set(&self, data: Arc<T>) -> Arc<T> {
-        let old_ptr = self.inner().set_ptr(Arc::into_raw(data));
-        unsafe { Arc::from_raw(old_ptr) }
+    pub fn set(&self, data: Box<T>) -> Box<T> {
+        let old_ptr = self.inner().set_ptr(Box::into_raw(data));
+        unsafe { Box::from_raw(old_ptr) }
     }
 
     #[inline]
-    pub fn get(&self) -> Arc<T> {
-        let x = unsafe { Arc::from_raw(self.inner().ptr()) };
-        let o = Arc::clone(&x);
-        std::mem::forget(x);
-        o
+    pub fn get(&self) -> ArcRef<T> {
+        self.inner().increment_strong();
+        ArcRef { ptr: self.ptr, phantom: self.phantom }
     }
 
     #[inline]
@@ -161,6 +165,33 @@ impl<T> ArcCell<T> {
     #[inline]
     fn inner(&self) -> &ArcCellInner<T> {
         unsafe { self.ptr.as_ref() }
+    }
+}
+
+impl<T> ArcRef<T> {
+    #[inline]
+    fn inner(&self) -> &ArcCellInner<T> {
+        unsafe { self.ptr.as_ref() }
+    }
+}
+
+impl<T: Display> Display for ArcRef<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(unsafe { &*self.inner().ptr() }, f)
+    }
+}
+
+impl<T: Debug> Debug for ArcRef<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Debug::fmt(unsafe { &*self.inner().ptr() }, f)
+    }
+}
+
+impl<T> Deref for ArcRef<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.inner().ptr() }
     }
 }
 
@@ -181,16 +212,16 @@ mod tests {
 
     #[test]
     fn big_test() {
-        let v = ArcCell::new(Arc::new("Something horrible".to_string()));
+        let v = ArcCell::new(Box::new("Something horrible".to_string()));
         let v0 = ArcCell::clone(&v);
         let t = std::thread::spawn(move || {
             println!("{}", v0.get());
             let v1 = ArcCell::clone(&v0);
             let _t = std::thread::spawn(move || {
                 println!("{}", v1.get());
-                v1.set(Arc::new("Some other value again heh".to_string()));
+                v1.set(Box::new("Some other value again heh".to_string()));
             });
-            v0.set(Arc::new("Some other value".to_string()));
+            v0.set(Box::new("Some other value".to_string()));
         });
         let v0 = ArcCell::clone(&v);
         let _t = std::thread::spawn(move || {
@@ -198,9 +229,9 @@ mod tests {
             let v1 = ArcCell::clone(&v0);
             let _t = std::thread::spawn(move || {
                 println!("{}", v1.get());
-                v1.set(Arc::new("Some other value again heh 1".to_string()));
+                v1.set(Box::new("Some other value again heh 1".to_string()));
             });
-            v0.set(Arc::new("Some other value".to_string()));
+            v0.set(Box::new("Some other value".to_string()));
         });
         let v0 = ArcCell::clone(&v);
         let _t = std::thread::spawn(move || {
@@ -208,9 +239,9 @@ mod tests {
             let v1 = ArcCell::clone(&v0);
             let _t = std::thread::spawn(move || {
                 println!("{}", v1.get());
-                v1.set(Arc::new("Some other value again heh 2".to_string()));
+                v1.set(Box::new("Some other value again heh 2".to_string()));
             });
-            v0.set(Arc::new("Some other value".to_string()));
+            v0.set(Box::new("Some other value".to_string()));
         });
         let v0 = ArcCell::clone(&v);
         let _t = std::thread::spawn(move || {
@@ -218,9 +249,9 @@ mod tests {
             let v1 = ArcCell::clone(&v0);
             let _t = std::thread::spawn(move || {
                 println!("{}", v1.get());
-                v1.set(Arc::new("Some other value again heh 3".to_string()));
+                v1.set(Box::new("Some other value again heh 3".to_string()));
             });
-            v0.set(Arc::new("Some other value".to_string()));
+            v0.set(Box::new("Some other value".to_string()));
         });
         let v0 = ArcCell::clone(&v);
         let _t = std::thread::spawn(move || {
@@ -228,9 +259,9 @@ mod tests {
             let v1 = ArcCell::clone(&v0);
             let _t = std::thread::spawn(move || {
                 println!("{}", v1.get());
-                v1.set(Arc::new("Some other value again heh 4".to_string()));
+                v1.set(Box::new("Some other value again heh 4".to_string()));
             });
-            v0.set(Arc::new("Some other value".to_string()));
+            v0.set(Box::new("Some other value".to_string()));
         });
         let v0 = ArcCell::clone(&v);
         let _t = std::thread::spawn(move || {
@@ -238,9 +269,9 @@ mod tests {
             let v1 = ArcCell::clone(&v0);
             let _t = std::thread::spawn(move || {
                 println!("{}", v1.get());
-                v1.set(Arc::new("Some other value again heh 5".to_string()));
+                v1.set(Box::new("Some other value again heh 5".to_string()));
             });
-            v0.set(Arc::new("Some other value".to_string()));
+            v0.set(Box::new("Some other value".to_string()));
         });
         let v0 = ArcCell::clone(&v);
         let _t = std::thread::spawn(move || {
@@ -248,9 +279,9 @@ mod tests {
             let v1 = ArcCell::clone(&v0);
             let _t = std::thread::spawn(move || {
                 println!("{}", v1.get());
-                v1.set(Arc::new("Some other value again heh 6".to_string()));
+                v1.set(Box::new("Some other value again heh 6".to_string()));
             });
-            v0.set(Arc::new("Some other value".to_string()));
+            v0.set(Box::new("Some other value".to_string()));
         });
         println!("A: {}", v.get());
         let _ = t.join();
